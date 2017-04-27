0dca4f21274cfd723afb0baebc03e9ce5dd201f2
BenoitArguel <benoit.arguel@gmail.com>
2017-04-27 15:17:01 +0200

    task: add static element for menu & info section

--- app/js/index.js
+++ app/js/index.js
@@ -1,8 +1,6 @@
 import $ from 'jquery';
 
 $('#menu').on('click', e => {
-  $('#menu').toggleClass('clicked');
-  $('#menu2').toggleClass('clicked');
-  $('#arrow-menu').toggleClass('clicked');
+  $(['#menu', '#menu2', '#arrow-menu']).toggleClass('clicked');
   e.preventDefault();
 });
